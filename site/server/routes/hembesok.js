adb8a1b733b595e439207e0158a0278d558bc3e3
PermittedCheetah <81412972+PermittedCheetah@users.noreply.github.com>
2021-04-14 14:03:07 +0200

    created post-request for hembesok

--- site/server/routes/hembesok.js
+++ site/server/routes/hembesok.js
@@ -6,12 +6,12 @@ module.exports = function(app, pool) {
                 `SELECT to_char(at_family, 'HH24:MI') AS at_family, 
                 to_char(from_family, 'HH24:MI') AS from_family, 
                 to_char(from_family, 'yyyy-mm-dd') AS date,
-                amining_nutrition,
+                amning_nutrition,
                 stodsamtal,
                 viktkontroll,
                 provtagning,
                 lakemedel,
-                annan_åt,
+                annan_at,
                 lakare,
                 logoped,
                 dietist,
@@ -29,4 +29,52 @@ module.exports = function(app, pool) {
             console.error(err);
         }
     })
+
+    //add 
+	app.post("/hembesok/:protokollnr", async(req, res) => {
+		try {
+			
+			const {protokollnr} = req.params;
+            console.log(protokollnr);
+            const {at_family, from_family, performed_by, amning_nutrition, stodsamtal, viktkontroll, provtagning, lakemedel,annan_at,lakare,logoped,
+                dietist,
+                annan_resurs,
+                av_logistik, 
+                av_barn_familj,
+                av_personal,
+                av_beskrivning} = req.body;
+			const newHembesok = await pool.query(
+			`INSERT INTO Hembesok (protokollnr, 
+                at_family, 
+                from_family, 
+                performed_by, 
+                amning_nutrition, 
+                stodsamtal, 
+                viktkontroll, 
+                provtagning, 
+                lakemedel,
+                annan_at,
+                lakare,
+                logoped,
+                dietist,
+                annan_resurs,
+                av_logistik, 
+                av_barn_familj,
+                av_personal,
+                av_beskrivning
+                ) 
+			VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)`, [protokollnr, 
+                at_family, from_family, performed_by, amning_nutrition, stodsamtal, viktkontroll, provtagning, lakemedel,annan_at,lakare,logoped,
+                dietist,
+                annan_resurs,
+                av_logistik, 
+                av_barn_familj,
+                av_personal,
+                av_beskrivning]);
+
+			res.json(newHembesok.rows);
+		} catch (err) {
+			console.error(err.message);
+		}
+	});
 }
