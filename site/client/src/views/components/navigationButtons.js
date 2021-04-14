e3390a64d02227072f231e661533ab4a170c3a91
Puzzel123 <jakobhenriksson@live.se>
2021-04-14 15:27:15 +0200

    fixed css for components

--- site/client/src/views/components/navigationButtons.js
+++ site/client/src/views/components/navigationButtons.js
@@ -1,35 +1,73 @@
 import React from 'react'
 
+import "./navigationButtons.css";
+
 
 const Navigation = (params) => {
     const { id } = params;
     const getNavigationHome = () => {
         return (
-            <button class="mybutton" onClick={() => {
+            <button class = "navigationButton"  onClick={() => {
                 window.location = "/home/";
             }}> Home </button>
         )
     }
+    const getNavigationInskrivning = () => {
+        return (
+            <button class = "navigationButton"  onClick={() => {
+                window.location = "/inskrivning/" + id;
+            }}> Inskrivning </button>
+        )
+    }
     const getNavigationHembesok = () => {
         return (
-            <button class="hembesok" onClick={() => {
+            <button class = "navigationButton"  onClick={() => {
                 window.location = "/hembesok/" + id;
             }}> Hembesok </button>
         )
     }
     const getNavigationDigitalaVardmot = () => {
         return (
-            <button class="digitalVard" onClick={() => {
+            <button class = "navigationButton"  onClick={() => {
                 window.location = "/digitalvardmote/" + id;
             }}> Digitalvardmote </button>
         )
     }
+    const getNavigationMottagningBesok = () => {
+        return (
+            <button class = "navigationButton" onClick={() => {
+                window.location = "/mottagnignsBesok/" + id;
+            }}> Mottagningsbesok </button>
+        )
+    }
+    const getNavigationAterlaggning = () => {
+        return (
+            <button class = "navigationButton" onClick={() => {
+                window.location = "/aterlaggning/" + id;
+            }}> Aterlaggning </button>
+        )
+    }
         return(
-            <div class="navigation"> 
-            {getNavigationHembesok()}
-            {getNavigationDigitalaVardmot()}
+         <div class="navigation"> 
+            
+            <div class = "inskrivningDiv">
+            {getNavigationInskrivning()}
+            </div>
+            <div class = "hembesokDiv">
             {getNavigationHembesok()}
             </div>
+            <div class = "digitalVard">
+            {getNavigationDigitalaVardmot()}
+            <div class = "mottagningDiv">
+            {getNavigationMottagningBesok()}
+            </div>
+            </div>
+            <div class = "aterlaggningDiv">
+            {getNavigationAterlaggning()}
+            </div>
+            
+
+          </div>
             
         );
     }
