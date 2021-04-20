d46eff5333b0841985f91290428720f9fb24c80d
jakob-ristner <jakob.o.ristner@gmail.com>
2021-04-20 10:23:54 +0200

    created form for editing inskrivning

--- site/client/src/views/editRegistration/EditRegistration.js
+++ site/client/src/views/editRegistration/EditRegistration.js
@@ -7,9 +7,38 @@ const EditRegistration = (useParams) => {
     
     return(
         <Fragment>
-            <div>
-                <h1>Redigera för {id}</h1>             
-            </div>
+            <form class="registration">
+                <div className="header">
+                Protokollnr: <input type="number"/><br />
+                Inskrivningsdatum: <input type="date" /> <br />
+                Anledning for inskrivning: <input type="text" />
+                </div>
+                <div className="bakgrundsdata">
+                Barnets gestationsvecka: <input type="number" /> <br />
+                Födelsevikt (gram): <input type="number" /><br />
+                Födelselängd (cm): <input type="number" /> <br />
+                Födelsehuvudomfång (cm): <input type="number" />
+                </div>
+                <div className="inskrivning">
+                Vikt (gram): <input type="number" /> <br />
+                Längd (cm): <input type="number" /> <br />
+                Huvudomfång (cm): <input type="number" /> <br />
+                Mamma vill amma: H<input type="checkbox" class="helt" /> 
+                                D<input type="checkbox" class="delvis"/>
+                                IA<input type="checkbox" class="inte"/> <br />
+                Erhåller Bröstmjölk: H<input type="checkbox" class="helt" /> 
+                                    D<input type="checkbox" class="delvis"/>
+                                    IA<input type="checkbox" class="inte"/> <br />
+                Barnet har v-sond: <input type="checkbox" class="nej" /> nej<input type="checkbox" class="ja"/><br />
+                Infart: <input type="text" /><br />
+                Andningsstöd: <input type="text" /> <br />
+                Extra Syrgasbehov: ja<input type="checkbox" class="nej" /> nej<input type="checkbox" class="ja"/><br />
+                </div>
+
+
+            </form>
+
+            <form class="discharge" action=""></form>
         </Fragment>
 
     );
