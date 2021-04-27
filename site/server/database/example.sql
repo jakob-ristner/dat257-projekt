9325c2bc71f16a7a59c715f1a6df945943b870fc
Puzzel123 <jakobhenriksson@live.se>
2021-04-27 09:29:01 +0200

    Fixed a single setup.sql for all sql files, Jakob & Nima

---
+++ site/server/database/example.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS Example;
+CREATE TABLE Example (
+    id CHAR(6) PRIMARY KEY,
+    dateOfBirth DATE NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO Example (id) VALUES ('111111');
+INSERT INTO Example (id, dateOfBirth) VALUES ('222222', '2020-05-02');
+INSERT INTO Example (id, dateOfBirth) VALUES ('333333', '1999-07-01');
+INSERT INTO Example (id, dateOfBirth) VALUES ('444444', '2001-03-26');
+INSERT INTO Example (id, dateOfBirth) VALUES ('555555', '1995-01-01');
+INSERT INTO Example (id, dateOfBirth) VALUES ('434512', '1995-01-01');
+
--- site/server/setup.sql
+++ site/server/setup.sql
@@ -1,13 +1,5 @@
-DROP TABLE IF EXISTS Example;
-CREATE TABLE Example (
-    id CHAR(6) PRIMARY KEY,
-    dateOfBirth DATE NOT NULL DEFAULT NOW()
-);
 
-INSERT INTO Example (id) VALUES ('111111');
-INSERT INTO Example (id, dateOfBirth) VALUES ('222222', '2020-05-02');
-INSERT INTO Example (id, dateOfBirth) VALUES ('333333', '1999-07-01');
-INSERT INTO Example (id, dateOfBirth) VALUES ('444444', '2001-03-26');
-INSERT INTO Example (id, dateOfBirth) VALUES ('555555', '1995-01-01');
-INSERT INTO Example (id, dateOfBirth) VALUES ('434512', '1995-01-01');
-
+\i database/in-out.sql 
+\i database/example.sql
+\i database/hemb.sql
+\i database/dvard.sql
