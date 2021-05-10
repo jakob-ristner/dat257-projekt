59bcac6b78bd4d8d23252d7ff37645f0494f06bd
jakob-ristner <jakob.o.ristner@gmail.com>
2021-05-10 15:07:34 +0200

    added form with states / JR, EB

--- site/client/src/views/mottagningsbesok/EditMott.js
+++ site/client/src/views/mottagningsbesok/EditMott.js
@@ -5,7 +5,7 @@ const EditMott = (useParams) => {
     const {id} = useParams.match.params;
 
     
-    
+        const [protokollnr, setProtokollnr] = useState();
         const [date, setDate] = useState();
         const [start_time, setStartTime] = useState();
         const [end_time, setEndTime] = useState();
@@ -25,11 +25,77 @@ const EditMott = (useParams) => {
         const [av_barn_familj, setAvBarnFamilj] = useState();
         const [av_personal, setAvPersonal] = useState();
         const [av_beskrivning, setAvBesk] = useState();
+
+    const submit = (e) => {
+        e.preventDefault();
+
+    }
     
         
-    return(
-        <h1>Mottagningsbesök!!!!!!!!!!!!!!!!</h1>
-    );
+    return(<Fragment>
+        <h1>Id: {id}</h1>
+        <button onClick={() => {window.location = "/digitalt-vardmote/" + protokollnr}}>Avbryt</button>
+        <form onSubmit={submit}>
+            Datum: <input required type="date" value={ date } 
+                onChange={(e) => {setDate(e.target.value)}}/> <br/>
+
+            Starttid: <input required type="time" value={ start_time } 
+                onChange={(e) => {setStartTime(e.target.value)}}/> <br/>
+
+            Sluttid: <input required type="time" value={ end_time } 
+                onChange={(e) => {setEndTime(e.target.value)}}/> <br/>
+
+            Utförd av: <input required type="text" value={ performed_by } 
+                onChange={(e) => {setPerformedBy(e.target.value)}}/> <br/>
+
+            Amning: <input type="checkbox" checked={ amning_nutrition } 
+                onChange={(e) => {setAmning(e.target.checked)}}/> <br/>
+
+            Stödsamtal: <input type="checkbox" checked={ stodsamtal } 
+                onChange={(e) => {setStodsamtal(e.target.checked)}}/> <br/>
+
+            Viktkontroll: <input type="checkbox" checked={ viktkontroll } 
+                onChange={(e) => {setViktkontroll(e.target.checked)}} /> <br/>
+
+            Provtagning: <input type="checkbox" checked={provtagning} 
+                onChange={(e) => {setProvtagning(e.target.checked)}} /> <br/>
+
+            Läkemedel: <input type="checkbox" checked={lakemedel} 
+                onChange={(e) => {setLakemedel(e.target.checked)}} /> <br/>
+                
+
+            Annan åtgärd: <input type="text" value={ annat_mote } 
+                onChange={(e) => {setAnnatMote(e.target.value)}}/> <br/>
+
+            Läkare: <input type="checkbox" checked={ lakare } 
+                onChange={(e) => {setLakare(e.target.checked)}}/> <br/>
+
+            Logoped <input type="checkbox" checked={ logoped } 
+                onChange={(e) => {setLogoped(e.target.checked)}}/> <br/>
+
+            Dietist <input type="checkbox" checked={ dietist } 
+                onChange={(e) => {setDietist(e.target.checked)}}/> <br/>
+            Kurator <input type="checkbox" checked={ kurator } 
+                onChange={(e) => {setKurator(e.target.checked)}}/> <br/>
+
+            Annan Resurs: <input type="text" value={ annan_resurs } 
+                onChange={(e) => {setAnnanResurs(e.target.value)}}/> <br/>
+            
+            {/* avvikelser*/}
+            Logistik <input type="checkbox" checked={av_logistik} 
+                onChange={(e) => {setAvLogistik(e.target.checked)}}/> <br/>
+            Barn/Familj <input type="checkbox" checked={ av_barn_familj } 
+                onChange={(e) => {setAvBarnFamilj(e.target.checked)}}/> <br/>
+            Personal <input type="checkbox" checked={av_personal} 
+                onChange={(e) => {setAvPersonal(e.target.checked)}}/> <br/>
+            Förklaring: <input type="text" value={av_beskrivning} 
+                onChange={(e) => {setAvBesk(e.target.value)}}/> <br/>
+
+            <input type="submit" value="Spara" />
+
+        </form>
+
+    </Fragment>);
 }
 
 export default EditMott;
