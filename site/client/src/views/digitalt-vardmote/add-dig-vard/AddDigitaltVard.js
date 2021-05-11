40495525470ce900bf8c7c3468e4263fe77cfa3e
MhdJamal <jamalbasal35@gmail.com>
2021-05-12 01:23:19 +0200

    Added css for AddDigitalVard

--- site/client/src/views/digitalt-vardmote/add-dig-vard/AddDigitaltVard.js
+++ site/client/src/views/digitalt-vardmote/add-dig-vard/AddDigitaltVard.js
@@ -70,15 +70,17 @@ const AddDigitaltVard = (useParams) => {
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(body)
             });
+
             await console.log(response);
         } catch (err) {
             console.error(err);
         }
     }
     
-    useEffect(() => {
+    /*useEffect(() => {
         validateAtgard()
     }, []);
+    */
 
     /*   Annat möte: Ja<input class="distance" type="checkbox" checked={annat_motes} onChange={(e) => {
                     set_annat_motes(e.target.checked)
@@ -102,24 +104,22 @@ const AddDigitaltVard = (useParams) => {
     return (
         <Fragment>
          <h1>Lägg till digitalt vårdmöte för {protocolID}</h1>
-         <button class = {layout.avbrytButton} onClick={() =>{window.location="/digitalt-vardmote/" + protocolID} }>Avbryt</button>
-        <div class = {layout.gridHalleluja}>
-        <div class = {layout.smallerBox}>
-            <div class = {layout.gridHeaders}>
-                <div>Välj tider:</div>
-                <hr class ={layout.line1}></hr>
-                <div>Välj Åtgärd:</div>
-                <hr class ={layout.line1}></hr>
-                <div>Välj Resurs:</div>
-                <hr class ={layout.line1}></hr>
-                <div>Välj Avvikning:</div>
-            </div>
-        </div>
-        <div class = {layout.test}>
+
+
 
     <form onSubmit={submit}>
-        <div class={layout.gridAdd}>
-            <div class={layout.info}>
+
+           <div class = {layout.gridHalleluja}>
+                <div><h2 className={layout.headerInfo}>Välj tider:</h2></div>
+
+                <div><h2 className={layout.headerAtgard}>Välj Åtgärd:</h2></div>
+
+                <div><h2 className={layout.headerResurs}>Välj Resurs:</h2></div>
+
+                <div> <h2 className={layout.headerAvvikning}>Välj Avvikning:</h2></div>
+
+
+                <div class={layout.info}>
                 <div class = {layout.gridInfo}>
                 <div>Datum utfört: <input required type="date" value={date} onChange={(e) => {set_date(e.target.value)}}></input></div>
                 <div>Start klockan: <input required type="time" value={start_time} onChange={(e) => {set_start_time(e.target.value)}}></input></div>
@@ -127,8 +127,10 @@ const AddDigitaltVard = (useParams) => {
                 <div>Utförd av: <input required placeholder="Sköterske-ID" type="text" value={performed_by} onChange={(e) => {set_performed_by(e.target.value)}}></input></div>
             </div>
         </div>
-            <div className = "atgard">
-            <div class= {layout.atgard}>
+
+
+            <div class = {layout.atgard}>
+            <div class= {layout.gridAtgard}>
                 <div><input class="distance" type="checkbox" checked={amning_nutrition} onChange={(e) => {set_amning_nutrition(e.target.checked)}}></input>Amning-/nutrionssamtal:</div>
                 <div><input class="distance"  type="checkbox" checked={stodsamtal} onChange={(e) => {set_stodsamtal(e.target.checked)}}></input>Stödsamtal:</div>
                 <div><input class="distance" type="checkbox" checked={viktkontroll} onChange={(e) => {set_viktkontroll(e.target.checked)}}></input>Viktkontroll: </div> 
@@ -145,33 +147,37 @@ const AddDigitaltVard = (useParams) => {
              </div>
             </div>
 
-             <div class= {layout.reurs}>
-                <div class= {layout.gridResurs}>
-                <input class="distance" type="checkbox" checked={lakare} onChange={(e) => {set_lakare(e.target.checked)}}></input>Läkare
-                <input class="distance" type="checkbox" checked={logoped} onChange={(e) => {set_logoped(e.target.checked)}}></input>Logoped
-                <input class="distance" type="checkbox" checked={dietist} onChange={(e) => {set_dietist(e.target.checked)}}></input> Dietist
-                <input class="distance" type="checkbox" checked={kurator} onChange={(e) => {set_kurator(e.target.checked)}}></input>Kurator
-                Annan resurs: <input type="text" value={annan_resurs} onChange={(e) => {set_annan_resurs(e.target.value)}}></input>
+             <div class = {layout.resurs}>
+             <div class= {layout.gridResurs}>
+
+                <div><input class="distance" type="checkbox" checked={lakare} onChange={(e) => {set_lakare(e.target.checked)}}></input>Läkare</div>
+                <div><input class="distance" type="checkbox" checked={logoped} onChange={(e) => {set_logoped(e.target.checked)}}></input>Logoped</div>
+                <div><input class="distance" type="checkbox" checked={dietist} onChange={(e) => {set_dietist(e.target.checked)}}></input> Dietist</div>
+                <div><input class="distance" type="checkbox" checked={kurator} onChange={(e) => {set_kurator(e.target.checked)}}></input>Kurator</div>
+                <div>Annan resurs: <input type="text" value={annan_resurs} onChange={(e) => {set_annan_resurs(e.target.value)}}></input></div>
                 
                 </div>
             </div>
            
             <div class= {layout.avvikning}>
-              <div class = {layout.gridAvvikning}> 
-               <input type = "checkbox" type="checkbox" checked={av_logistik} onChange={(e) => {set_av_logistik(e.target.checked)}}></input>Kurator
-              <input type = "checkbox" type="checkbox" checked={av_barn_familj} onChange={(e) => {set_av_barn_familj(e.target.checked)}}></input>Kurator
-              <input type = "checkbox" type="checkbox" checked={av_personal} onChange={(e) => {set_av_personal(e.target.checked)}}></input>Kurator
-              Annan resurs: <input type="text" value={av_beskrivning} onChange={(e) => {set_av_besk(e.target.value)}}></input>
+            <div class = {layout.gridAvvikning}>
+ 
+            <div><input type = "checkbox" type="checkbox" checked={av_logistik} onChange={(e) => {set_av_logistik(e.target.checked)}}></input>Kurator</div>
+            <div><input type = "checkbox" type="checkbox" checked={av_barn_familj} onChange={(e) => {set_av_barn_familj(e.target.checked)}}></input>Kurator</div>
+            <div><input type = "checkbox" type="checkbox" checked={av_personal} onChange={(e) => {set_av_personal(e.target.checked)}}></input>Kurator</div>
+            <div>Annan resurs: <input type="text" value={av_beskrivning} onChange={(e) => {set_av_besk(e.target.value)}}></input></div>
               </div>
             </div>
-
+        
+        <div class = {layout.divButton}>
         <button class= {layout.saveButton}>Spara</button>
+        <button class = {layout.avbrytButton} onClick={() =>{window.location="/digitalt-vardmote/" + protocolID} }>Avbryt</button>
+        
         </div>
-        
-        </form>
-        
-           </div>
-           </div>
+        </div>
+
+    </form>
+
          </Fragment>
     );
 }
