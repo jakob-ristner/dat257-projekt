a84da33372e3525d17faf28c02c1dde0a7119a89
PermittedCheetah <81412972+PermittedCheetah@users.noreply.github.com>
2021-05-21 14:47:33 +0200

    homepage validate now works as intended and displays an error if patient is not in the database

--- site/client/src/views/homepage/Homepage.js
+++ site/client/src/views/homepage/Homepage.js
@@ -3,117 +3,109 @@ import Navigation from "../components/navigationButtons";
 //import { createPopper } from '@popperjs/core';
 import {Dropdown} from 'react-bootstrap';
 import {DropdownButton} from 'react-bootstrap';
-import { useAlert } from 'react-alert';
-import axios from 'axios';
 
 const Homepage = () => {
 
     const [protokollnr, set_protokollnr] = useState("");
-    const [navigate, set_navigate] = useState("");
+    const [navigate, set_navigate] = useState("registration");
     const [list, setList] = useState([]);
-
-    //const alert = useAlert();
-
+    
     const setNavigate = (useParams) => {
         set_navigate(useParams);
     }
-
+    
+    //lägg till: , { credentials: 'include'} i fetchen
+    //Http get-request that fetches all protocolID:s and puts them in a list
     const getPro = async() => {
-       // axios.get("http://localhost:5000/startsida")
-       // .then(result=>{setList(result.data)});
-        
-         
-        //e.preventDefault();
          try {
              const response =  await fetch(
              "http://localhost:5000/startsida");
              const jsonData =  await response.json()
-             /*
-             .then(response => response.json())
-             .then(console.log("Fetched resource"))
-             .then(data => this.setState({ data }));
-             */
-             //const list = jsonData[0];
              setList(jsonData);
              console.log(jsonData);
              console.log(list);
          } catch(err) {
-            // console.log(list);
              console.error(err);
          }
          
      }
-     /*
-     useEffect(() => {getPro(); console.log(list)
-     }, []);
- 
-     */
- 
-     useEffect(() => {getPro(); console.log(list);
-     }, []);
-
-    const validattt = () => {
-        var pro = document.getElementsByClassName("pro").item(0);
-        var input = pro.getElementsByTagName("input");
-        var valid = false;
+   
+    //Method checks that the input protocolID is in the database. If yes, it routes to the correct location
+    //If no, a message is displayed that informs the user
+    const validateProtocolID = () => {
         
         for(var i = 0; i < list.length; i++){
-            if (list[i].protocolid != protokollnr || navigate == ""){
-                console.log(list[i].protocolid);
-                //input.setCustomValidity("Används ej");
-                //pro.getElementsByTagName("input").setCustomValidity("Används ej");
-                console.log("nej");
-               // alert.show("NEJ");
+            if (list[i].protocolid != protokollnr){
+                document.getElementById("pro").setCustomValidity("Patienten finns ej");
+                console.log("hello");  
             }
             else {
-                //input.item(0).setCustomValidity("");
                 console.log("ja");  
                 window.location="/"+ navigate + "/" + protokollnr  
-                valid = true;
             }
-            console.log(valid);
+           
+        }
+    }
+    
+    useEffect(() => {getPro(); console.log(list); validateProtocolID();
+    }, []);
+
+    //the method adjust the choosen form and displays it so the user is well aware of their choice
+    const title = () => {
+        if (navigate == "registration"){
+            return "In-/utskrivning"
+        }
+        else if (navigate == "hembesok"){
+            return "Hembesök"
+        }
+        else if (navigate == "?"){
+            return "Återläggning"
+        }
+        else if (navigate == "undersokning"){
+            return "Undersökning"
+        }
+        else if (navigate == "digitalt-vardmote"){
+            return "Digitalt vårdmöte"
+        }
+        else if (navigate == "mottagningsbesok"){
+            return "Mottagningsbesök"
         }
     }
 
-    const validateProtocolID = () => {
-        //var pro = document.getElementsByClassName("pro");
-        //console.log(navigate);
-        if (protokollnr !== ""){
-            window.location="/"+ navigate + "/" + protokollnr
-        }
-        if (protokollnr == ""  || navigate == "") {
-            window.location="/"; 
-        }
-    }
-
+    //Displays the homepage of the application
+    //The user can input a patient ID and select which type of form to go to
+    //Or create a new patient ID
     return (
         <Fragment>
             
-                <div className="container">
-                    <Navigation id={111}/>
-                    <h1> Startsida </h1>
-                </div>
+               
+            <h1> Startsida </h1>
+        
                 
-                <div className="pro">    
-                Protokollnummer: <input required type="text" placeholder="Patient ID" value={protokollnr} onChange={(e) => {set_protokollnr(e.target.value)}}></input>
+            <form onSubmit={validateProtocolID}>
+                <div>    
+                Protokollnummer: <input required type="text" id="pro" placeholder="Patient ID" value={protokollnr} onChange={(e) => {set_protokollnr(e.target.value)}}></input>
                 
                 <button onClick={() => 
-                    {validattt()}}>SÖK</button>
+                    {validateProtocolID()}}>SÖK</button>
                 
                 <button onClick={() => 
                     {window.location="/registration/"}}>Lägg till en ny patient</button>
                 </div>
-            <DropdownButton id="dropdown-basic-button" title="Välj">
-                <Dropdown.Item value={navigate}><button onClick={() => setNavigate("registration")}>In-/utskrivning</button></Dropdown.Item><br/>
-                <Dropdown.Item value={navigate}><button onClick={() => setNavigate("hembesok")}>Hembesök</button></Dropdown.Item><br/>
-                <Dropdown.Item value={navigate}><button onClick={() => setNavigate("mottagningsbesok")}>Mottagningsbesök</button></Dropdown.Item><br/>
-                <Dropdown.Item value={navigate}><button onClick={() => setNavigate("?")}>Återläggning</button></Dropdown.Item><br/>
-                <Dropdown.Item value={navigate}><button onClick={() => setNavigate("undersokning")}>Undersökning</button></Dropdown.Item><br/>
-                <Dropdown.Item value={navigate}><button onClick={() => setNavigate("digitalt-vardmote")}>Digitalt vårdmöte</button></Dropdown.Item>
-            </DropdownButton>
+               <div>
+                Du har valt:<DropdownButton id="dropdown-basic-button" title={title()}><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("registration")}>In-/utskrivning</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("hembesok")}>Hembesök</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("mottagningsbesok")}>Mottagningsbesök</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item id="ater" value={navigate}><button onClick={() => setNavigate("?")}>Återläggning</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("undersokning")}>Undersökning</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("digitalt-vardmote")}>Digitalt vårdmöte</button></Dropdown.Item>
+                </DropdownButton>
+                </div>
 
+            </form>
             
-
+            
 
         </Fragment>
     );
@@ -122,14 +114,21 @@ const Homepage = () => {
 export default Homepage;
 
 
-/*<Dropdown>
-                <Dropdown.Toggle style="width: 120px" variant="success" id="dropdown-basic">
-                    Dropdown Button
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>*/ 
+/* 
+<label>Choose a car:</label>
+<select >
+                    <option onClick={() => setNavigate("registration")} >In-/utskrivning</option>
+                    <option onClick={() => setNavigate("hembesok")}>Hembesök</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+/*
+<DropdownButton id="dropdown-basic-button" title={title()}><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("registration")}>In-/utskrivning</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("hembesok")}>Hembesök</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("mottagningsbesok")}>Mottagningsbesök</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("?")}>Återläggning</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("undersokning")}>Undersökning</button></Dropdown.Item><br/><br/>
+                    <Dropdown.Item value={navigate}><button onClick={() => setNavigate("digitalt-vardmote")}>Digitalt vårdmöte</button></Dropdown.Item>
+                </DropdownButton>
+                */
